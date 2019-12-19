0c53bdc5bf5c41c6eee866c3f33cb328fb2187f5
Babatunde <babatundea15@gmail.com>
2019-12-19 16:55:11 +0100

    I created and finished styling the landing page

--- src/Components/Landingpage.js
+++ src/Components/Landingpage.js
@@ -7,11 +7,31 @@ export class Resume extends Component {
       <div style={{width: '100%', margin: 'auto'}}>
           <Grid className="landing-grid">
               <Cell col={12}>
-                  <img src="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png"
+                  <img src="/Picture/FB_IMG_1547488634104.jpg"
                   alt="avatar"
                   className="avatar-img"/>
                   <div className="banner-text">
                       <h1>Full Stack Web Developer</h1>
+                      <hr/>
+                      <p>HTML | CSS | React | REDUX | Bootstrap | JavaScript</p>
+                      {/* LINKS TO MY SOCIAL PROFILE */}
+                  </div>
+                  <div className="social-links">
+                      {/* LINKEDIN */}
+                      <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                          <i className="fa fa-linkedin-square" aria-hidden="true" />
+                      </a>
+                      {/* GITHUB */}
+
+                      <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                          <i className="fa fa-github-square" aria-hidden="true" />
+                      </a>
+                    {/* TWITTER */}
+                      <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+                          <i className="fa fa-twitter-square" aria-hidden="true" />
+                      </a>
+
+
                   </div>
               </Cell>
           </Grid>
